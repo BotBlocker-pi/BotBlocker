6fcbdb278ee748b5c4c5a104ce39ad84145c4828
JoaoCLS <joao.carlos.santos@ua.pt>
2025-03-17 19:31:16 +0000

    POST - evaluation creation

--- Backend/app/views.py
+++ Backend/app/views.py
@@ -3,6 +3,11 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from app.dtos import *
 from app.models import *
+from app.serializers import *
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.utils.dateparse import parse_datetime
+
 
 
 def create_profile(username, platform, image=None):
@@ -30,3 +35,20 @@ def get_probability(request):
 
     serializer = ProfileDTO(username, platform)
     return JsonResponse(serializer.initial_data, safe=False)
+
+@csrf_exempt
+def criar_avaliacao(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  
+
+            serializer = EvaluationSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=400)
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
