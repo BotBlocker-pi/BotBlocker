6703d093698d5166495521b7ea82badd37606196
hujuc <hugocastro@ua.pt>
2025-03-18 16:53:04 +0000

    add Docker setup for React + Vite dashboard

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b7@)5f@jl&k4*^!d&#4+-qi@@#0d)4oo(ig+4fdzi1#9%u$g5$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
